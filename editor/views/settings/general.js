ccfbab24349d3bfa8f0e73cba6ef8d6d7f70201f
Stefan de Jong <stefan.de.jong@spectrumbroad.com>
2017-03-24 16:46:16 +0100

    editor/settings/general view update

--- editor/views/settings/general.js
+++ editor/views/settings/general.js
@@ -6,13 +6,31 @@ View.routes['/settings/general'] = function(EL) {
 		<section id="webserver">
 			<h2>Webserver</h2>
 			<dl>
+
 				<dt>
 					<label for="settingsGeneralWebserverPort">
 						Port
-						<div>The port this interface and the API routes are hosted on.</div>
+						<div>The plain-HTTP (non-SSL) port this web interface and the API routes are hosted on. If SSL is enabled, requests from this port are redirected to a secure connection on this port number incremented by 1.<br/>For example, if port 9600 is configured here and SSL is enabled, port 9601 will host the secure connection.</div>
 					</label>
 				</dt>
 				<dd><input id="settingsGeneralWebserverPort" type="number" value="9600" /></dd>
+
+				<dt>
+					<label for="settingsGeneralWebserverKeyPath">
+						SSL key path
+						<div>Path to a SSL key file. If configured together with the &quot;SSL certificate path&quot;, SSL will be enabled and plain HTTP requests redirect to the HTTPS connection.</div>
+					</label>
+				</dt>
+				<dd><input id="settingsGeneralWebserverKeyPath" type="text" /></dd>
+
+				<dt>
+					<label for="settingsGeneralWebserverCertPath">
+						SSL certificate path
+						<div>Path to a SSL certificate file. If configured together with the &quot;SSL key path&quot;, SSL will be enabled and plain HTTP requests redirect to the HTTPS connection.</div>
+					</label>
+				</dt>
+				<dd><input id="settingsGeneralWebserverCertPath" type="text" /></dd>
+
 			</dl>
 		</section>
 	</section>
