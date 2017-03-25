d964559f30a1500b123d935ada404f7308699110
Stefan de Jong <stefan.de.jong@spectrumbroad.com>
2017-03-25 21:37:19 +0100

    routes for setValue() and deleteValue()

--- app/Config/routes.js
+++ app/Config/routes.js
@@ -1,6 +1,31 @@
 module.exports = function(Config, XIBLE, EXPRESS_APP) {
 
 	EXPRESS_APP.get('/api/config', (req, res) => {
+		res.json(Config.getAll());
+	});
+
+	EXPRESS_APP.put('/api/config/value', (req, res) => {
+
+		let path = req.body.path;
+		let value = req.body.value;
+		if (typeof path !== 'string' || typeof value === 'undefined') {
+			return res.status(400).end();
+		}
+
+		Config.setValue(path, value);
+		res.end();
+
+	});
+
+	EXPRESS_APP.delete('/api/config/value', (req, res) => {
+
+		let path = req.body.path;
+		if (typeof path !== 'string') {
+			return res.status(400).end();
+		}
+
+		Config.deleteValue(path);
+		res.end();
 
 	});
 
