38f0e8b2355d3aca1c1b5a5315e99a859bf75202
Stefan de Jong <stefan.de.jong@spectrumbroad.com>
2017-03-25 21:35:35 +0100

    xibleWrapper update

--- editor/js/xibleWrapper.js
+++ editor/js/xibleWrapper.js
@@ -3,16 +3,39 @@ module.exports = function(XIBLE) {
 
 	class Config {
 
-    static validatePermissions() {
+		static validatePermissions() {
 
-      let req = XIBLE.httpBase.request('GET', `http${XIBLE.baseUrl}/api/config/validatePermissions`);
-      return req.toJson();
+			let req = XIBLE.httpBase.request('GET', `http${XIBLE.baseUrl}/api/config/validatePermissions`);
+			return req.toJson();
 
-    }
+		}
 
-  }
+		static deleteValue(path) {
 
-  return Config;
+			let req = XIBLE.httpBase.request('DELETE', `http${XIBLE.baseUrl}/api/config/value`);
+			return req.send({
+				path: path
+			});
+
+		}
+
+		static setValue(path, value) {
+
+			if (['string', 'number', 'boolean', 'date'].indexOf(typeof value) === -1) {
+				throw new Error(`Param "value" should be of type "string", "number", "boolean" or "date"`);
+			}
+
+			let req = XIBLE.httpBase.request('PUT', `http${XIBLE.baseUrl}/api/config/value`);
+			return req.send({
+				path: path,
+				value: value
+			});
+
+		}
+
+	}
+
+	return Config;
 
 };
 
