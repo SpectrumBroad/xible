8d4f5341b124d82e0d7ac896b3450b3a6c628679
Stefan de Jong <stefan.de.jong@spectrumbroad.com>
2017-03-25 21:37:06 +0100

    working setValue() & deleteValue()

--- app/Config/index.js
+++ app/Config/index.js
@@ -23,7 +23,23 @@ module.exports = function(XIBLE, EXPRESS_APP, CONFIG_OBJ) {
 
 	}
 
+	function saveConfig(path, configContents) {
+
+		configContents = JSON.stringify(configContents);
+
+		try {
+			fsExtra.writeFileSync(path, configContents);
+		} catch (err) {
+
+			configDebug(`failed to write config to "${path}": ${err}`);
+			throw new Error(`failed to write config to "${path}": ${err}`);
+
+		}
+
+	}
+
 	let loadTries = -1;
+
 	function loadConfig(path) {
 
 		configDebug(`loading "${path}"`);
@@ -53,11 +69,14 @@ module.exports = function(XIBLE, EXPRESS_APP, CONFIG_OBJ) {
 
 	}
 
-	let config;
+	let config, configPath;
 	if (CONFIG_OBJ) {
 		config = CONFIG_OBJ;
 	} else if (XIBLE.configPath) {
+
+		configPath = XIBLE.configPath;
 		config = loadConfig(XIBLE.configPath);
+
 	} else {
 		throw new Error(`need a configPath`);
 	}
@@ -90,11 +109,70 @@ module.exports = function(XIBLE, EXPRESS_APP, CONFIG_OBJ) {
 
 		}
 
+		/**
+		 * Deletes a value from the configuration.
+		 * Results are written of the the config path if that's how xible was loaded.
+		 * @param {String} path the json path, dot notated, in the config.
+		 * @returns {Boolean} boolean indicating whether indeed something was deleted.
+		 */
 		static deleteValue(path) {
+
+			let pathSplit = path.split('.');
+			let sel = config;
+
+			for (let i = 0; i < pathSplit.length - 1; ++i) {
+
+				let part = pathSplit[i];
+				if (sel.hasOwnProperty(part)) {
+					sel = sel[part];
+				} else {
+					return false;
+				}
+
+			}
+
+			delete sel[pathSplit.pop()];
+
+			if (configPath) {
+				saveConfig(configPath, config);
+			}
+
+			return true;
 
 		}
 
+		/**
+		 * Sets a value in the configuration.
+		 * If any part of the path does not exist, it is created.
+		 * Results are written of the the config path if that's how xible was loaded.
+		 * @param {String} path the json path, dot notated, in the config.
+		 * @param {String|Number|Boolean|Date} value the value to set
+		 */
 		static setValue(path, value) {
+
+			if (['string', 'number', 'boolean', 'date'].indexOf(typeof value) === -1) {
+				throw new Error(`Param "value" should be of type "string", "number", "boolean" or "date"`);
+			}
+
+			let pathSplit = path.split('.');
+			let sel = config;
+
+			for (let i = 0; i < pathSplit.length - 1; ++i) {
+
+				let part = pathSplit[i];
+				if (sel.hasOwnProperty(part)) {
+					sel = sel[part];
+				} else {
+					sel = sel[part] = {};
+				}
+
+			}
+
+			sel[pathSplit.pop()] = value;
+
+			if (configPath) {
+				saveConfig(configPath, config);
+			}
 
 		}
 
