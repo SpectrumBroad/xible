36bf1b8bbaecbbffed0647e44b2602d77add07ff
Stefan de Jong <stefan.de.jong@spectrumbroad.com>
2017-03-26 13:16:21 +0200

    improved interface unavailable warning

--- editor/views/settings/general.js
+++ editor/views/settings/general.js
@@ -7,7 +7,7 @@ View.routes['/settings/general'] = function(EL) {
 			<h2>Webserver</h2>
 			<p class="warning">
 				Changing any if the values in this section requires a restart of XIBLE.<br/>
-				Also note that after a restart as a consequence of these changes, the web interface will no longer be available on the current URL.
+				Also note that after a restart as a consequence of these changes, the web interface may no longer be available on the current URL.
 			</p>
 			<dl>
 
