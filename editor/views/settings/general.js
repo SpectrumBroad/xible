82e5d198ff7fcc39293e8dea1e02bea034545a0c
Stefan de Jong <stefan.de.jong@spectrumbroad.com>
2017-03-26 12:24:08 +0200

    added configpaths for working ui

--- editor/views/settings/general.js
+++ editor/views/settings/general.js
@@ -13,7 +13,7 @@ View.routes['/settings/general'] = function(EL) {
 						<div>The plain-HTTP (non-SSL) port this web interface and the API routes are hosted on. If SSL is enabled, requests from this port are redirected to a secure connection on this port number incremented by 1.<br/>For example, if port 9600 is configured here and SSL is enabled, port 9601 will host the secure connection.</div>
 					</label>
 				</dt>
-				<dd><input id="settingsGeneralWebserverPort" type="number" value="9600" /></dd>
+				<dd><input id="settingsGeneralWebserverPort" type="number" data-configpath="webServer.port" /></dd>
 
 				<dt>
 					<label for="settingsGeneralWebserverKeyPath">
@@ -21,7 +21,7 @@ View.routes['/settings/general'] = function(EL) {
 						<div>Path to a SSL key file. If configured together with the &quot;SSL certificate path&quot;, SSL will be enabled and plain HTTP requests redirect to the HTTPS connection.</div>
 					</label>
 				</dt>
-				<dd><input id="settingsGeneralWebserverKeyPath" type="text" /></dd>
+				<dd><input id="settingsGeneralWebserverKeyPath" type="text" data-configpath="webServer.keyPath" /></dd>
 
 				<dt>
 					<label for="settingsGeneralWebserverCertPath">
@@ -29,7 +29,7 @@ View.routes['/settings/general'] = function(EL) {
 						<div>Path to a SSL certificate file. If configured together with the &quot;SSL key path&quot;, SSL will be enabled and plain HTTP requests redirect to the HTTPS connection.</div>
 					</label>
 				</dt>
-				<dd><input id="settingsGeneralWebserverCertPath" type="text" /></dd>
+				<dd><input id="settingsGeneralWebserverCertPath" type="text" data-configpath="webServer.certPath" /></dd>
 
 			</dl>
 		</section>
