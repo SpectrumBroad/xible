5226eab233496c1b5c601fa81b21f4e9e45cff34
Stefan de Jong <stefan.de.jong@spectrumbroad.com>
2017-08-16 20:12:14 +0200

    expose timing object on flow

--- nodes/xible/xible.flow/index.js
+++ nodes/xible/xible.flow/index.js
@@ -1,25 +1,39 @@
 'use strict';
 
+function getFlow(flowId, callback) {
+  let messageHandler = (message) => {
+    if (message.flowId !== flowId || message.method !== 'returnFlow') {
+      return;
+    }
+    process.removeListener('message', messageHandler);
+    messageHandler = null;
+
+    callback(message.flow);
+  };
+
+  process.on('message', messageHandler);
+
+  process.send({
+    method: 'getFlowById',
+    flowId
+  });
+}
+
 module.exports = (NODE) => {
   const flowOut = NODE.getOutputByName('flow');
   flowOut.on('trigger', (conn, state, callback) => {
     const flowId = NODE.data.flowName || NODE.flow.name;
+    getFlow(flowId, callback);
+  });
 
-    let messageHandler = (message) => {
-      if (message.flowId !== flowId || message.method !== 'returnFlow') {
+  const timingOut = NODE.getOutputByName('timing');
+  timingOut.on('trigger', (conn, state, callback) => {
+    const flowId = NODE.data.flowName || NODE.flow.name;
+    getFlow(flowId, (flow) => {
+      if (!flow) {
         return;
       }
-      process.removeListener('message', messageHandler);
-      messageHandler = null;
-
-      callback(message.flow);
-    };
-
-    process.on('message', messageHandler);
-
-    process.send({
-      method: 'getFlowById',
-      flowId
+      callback(flow.timing);
     });
   });
 };
