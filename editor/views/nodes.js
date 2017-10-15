c944887d5962afb5819e4f726bcae12455915e0b
Stefan de Jong <stefan.de.jong@spectrumbroad.com>
2017-10-15 16:01:33 +0200

    playing around with future nodepack/nodes view.

--- editor/views/nodes.js
+++ editor/views/nodes.js
@@ -1,6 +1,6 @@
 'use strict';
 
-View.routes['/nodes'] = (EL) => {
+View.routes['/nodes'] = async (EL) => {
   EL.innerHTML = `
     <div id="sub">
       <header>XIBLE</header>
@@ -11,6 +11,37 @@ View.routes['/nodes'] = (EL) => {
         </ul>
       </section>
     </div>
-    <div class="inner"><h1></h1></div>
+    <div class="inner">
+      <section>
+        <h1>Nodepacks</h1>
+        <ul id="nodepacks">
+        </ul>
+      </section>
+    </div>
   `;
+
+  const nodePackUl = document.getElementById('nodepacks');
+
+  const nodePacks = await xibleWrapper.NodePack.getAll();
+  if (!nodePacks.length) {
+
+  }
+
+  for (let i = 0; i < nodePacks.length; i += 1) {
+    nodePackUl.appendChild(createNodePackEl(nodePacks[i]));
+  }
 };
+
+function createNodePackEl(nodePack) {
+  const li = document.createElement('li');
+
+  li.appendChild(document.createElement('h1'))
+  .appendChild(document.createTextNode(nodePack.name));
+
+  if (nodePack.description) {
+    li.appendChild(document.createElement('p'))
+    .appendChild(document.createTextNode(nodePack.description));
+  }
+
+  return li;
+}
