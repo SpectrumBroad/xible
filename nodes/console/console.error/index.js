94650a9c106f8f21f35fd4971c16399008174d2f
Jong <MV96TI@ad.ing.net>
2018-05-09 21:05:36 +0200

    async/await

--- nodes/console/console.error/index.js
+++ nodes/console/console.error/index.js
@@ -18,22 +18,22 @@ function log(str, NODE) {
 
 module.exports = (NODE) => {
   const triggerIn = NODE.getInputByName('trigger');
-  const doneOut = NODE.getOutputByName('done');
-  triggerIn.on('trigger', (conn, state) => {
-    const valueInput = NODE.getInputByName('value');
+  const valueIn = NODE.getInputByName('value');
 
-    if (!valueInput.isConnected()) {
+  const doneOut = NODE.getOutputByName('done');
+
+  triggerIn.on('trigger', async (conn, state) => {
+    if (!valueIn.isConnected()) {
       log(NODE.data.value || '', NODE);
       doneOut.trigger(state);
       return;
     }
 
-    valueInput.getValues(state).then((strs) => {
-      strs.forEach((str) => {
-        log(str, NODE);
-      });
-
-      doneOut.trigger(state);
+    const strs = await valueIn.getValues(state)
+    strs.forEach((str) => {
+      log(str, NODE);
     });
+
+    doneOut.trigger(state);
   });
 };
