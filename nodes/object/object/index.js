6d6ee608fa6702af22594e67c1a9fe7e2fb9af90
Jong <MV96TI@ad.ing.net>
2018-06-04 15:24:34 +0200

    support json for constructing objects
    This adds a textarea to the object node which allows setting JSON for the object.

--- nodes/object/object/index.js
+++ nodes/object/object/index.js
@@ -7,7 +7,16 @@ module.exports = (NODE) => {
   objOut.on('trigger', async (conn, state) => {
     const variables = await variablesIn.getValues(state);
 
-    const obj = {};
+    const json = NODE.data.json;
+    let obj = {};
+    if (json) {
+      try {
+        obj = JSON.parse(json);
+      } catch (err) {
+        NODE.error(err, state);
+      }
+    }
+
     variables.forEach((variable) => {
       let val = variable.values;
       if (val.length === 1) {
