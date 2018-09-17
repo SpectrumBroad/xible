9ded4cd57e83d784a56b714e6b3545a0f014eeb5
Jong <Stefan.de.Jong.wia@ing.com>
2018-09-17 14:39:37 +0200

    closes #77
    In conjunction with the fix for https://github.com/SpectrumBroad/xible-wrapper/issues/1

--- editor/js/xibleEditorFlow.js
+++ editor/js/xibleEditorFlow.js
@@ -1,112 +1,128 @@
 'use strict';
 
-class XibleEditorFlow extends xibleWrapper.Flow {
-  constructor(obj) {
-    super(obj);
+const XibleEditorFlow = (() => {
+  const constructed = {};
 
-    // set global appropriately when it's changed
-    this.on('global', (output) => {
-      this.setGlobalFromOutput(output);
-    });
-  }
+  class XibleEditorFlow extends xibleWrapper.Flow {
+    constructor(obj) {
+      super(obj);
 
-  removeGlobalConnectors() {
-    this.nodes.forEach((node) => {
-      node.getOutputs().forEach((output) => {
-        output.connectors.filter(conn => conn.global)
-        .forEach(globalConn => globalConn.delete());
+      if (obj && obj._id && this.constructor === XibleEditorFlow) {
+        if (constructed[obj._id]) {
+          return constructed[obj._id];
+        }
+
+        if (!constructed[obj._id]) {
+          constructed[obj._id] = this;
+        }
+      }
+
+      // set global appropriately when it's changed
+      this.on('global', (output) => {
+        this.setGlobalFromOutput(output);
       });
-    });
-  }
-
-  setGlobalFromOutput(output) {
-    // if we have another global of this type, ignore
-    let globalOutputsByType = this.getGlobalOutputs()
-    .filter(gOutput => gOutput.type === output.type);
-    if (
-      (!output.global && globalOutputsByType.length) ||
-      (output.global && globalOutputsByType.length > 1)
-    ) {
-      return;
     }
-    globalOutputsByType = null;
 
-    for (let i = 0; i < this.nodes.length; i += 1) {
-      this.nodes[i].getInputs().forEach((input) => {
-        if (
-          input.type === output.type && !input.connectors.filter(conn => !conn.global).length &&
-          input.global !== false
-        ) {
-          input.setGlobal(output.global ? true : undefined);
+    removeGlobalConnectors() {
+      this.nodes.forEach((node) => {
+        node.getOutputs().forEach((output) => {
+          output.connectors.filter(conn => conn.global)
+          .forEach(globalConn => globalConn.delete());
+        });
+      });
+    }
+
+    setGlobalFromOutput(output) {
+      // if we have another global of this type, ignore
+      let globalOutputsByType = this.getGlobalOutputs()
+      .filter(gOutput => gOutput.type === output.type);
+      if (
+        (!output.global && globalOutputsByType.length) ||
+        (output.global && globalOutputsByType.length > 1)
+      ) {
+        return;
+      }
+      globalOutputsByType = null;
+
+      for (let i = 0; i < this.nodes.length; i += 1) {
+        this.nodes[i].getInputs().forEach((input) => {
+          if (
+            input.type === output.type && !input.connectors.filter(conn => !conn.global).length &&
+            input.global !== false
+          ) {
+            input.setGlobal(output.global ? true : undefined);
+          }
+        });
+      }
+    }
+
+    stopAllInstances() {
+      const stopPromise = super.stopAllInstances();
+      this.undirect();
+      return stopPromise;
+    }
+
+    initNodes(nodes) {
+      this.nodes = [];
+      nodes.forEach(node => this.addNode(new XibleEditorNode(node)));
+    }
+
+    initConnectors(connectors) {
+      this.connectors = [];
+      connectors.forEach((conn) => {
+        conn.origin = this.getOutputById(conn.origin);
+        conn.destination = this.getInputById(conn.destination);
+
+        this.addConnector(new XibleEditorConnector(conn));
+      });
+    }
+
+    undirect() {
+      if (this.directedInstance) {
+        this.directedInstance.stop();
+      }
+
+      this.nodes.forEach((node) => {
+        node.element.classList.remove('nodirect');
+
+        if (node._directSetDataListener) {
+          node.removeListener('setdata', node._directSetDataListener);
+          delete node._directSetDataListener;
         }
       });
-    }
-  }
 
-  stopAllInstances() {
-    const stopPromise = super.stopAllInstances();
-    this.undirect();
-    return stopPromise;
-  }
+      this.connectors.forEach((connector) => {
+        connector.element.classList.remove('nodirect');
+      });
 
-  initNodes(nodes) {
-    this.nodes = [];
-    nodes.forEach(node => this.addNode(new XibleEditorNode(node)));
-  }
-
-  initConnectors(connectors) {
-    this.connectors = [];
-    connectors.forEach((conn) => {
-      conn.origin = this.getOutputById(conn.origin);
-      conn.destination = this.getInputById(conn.destination);
-
-      this.addConnector(new XibleEditorConnector(conn));
-    });
-  }
-
-  undirect() {
-    if (this.directedInstance) {
-      this.directedInstance.stop();
+      this.directedInstance = null;
     }
 
-    this.nodes.forEach((node) => {
-      node.element.classList.remove('nodirect');
-
-      if (node._directSetDataListener) {
-        node.removeListener('setdata', node._directSetDataListener);
-        delete node._directSetDataListener;
+    async direct(directNodes) {
+      if (!this.directedInstance) {
+        this.directedInstance = await this.createInstance();
       }
-    });
 
-    this.connectors.forEach((connector) => {
-      connector.element.classList.remove('nodirect');
-    });
-
-    this.directedInstance = null;
-  }
-
-  async direct(directNodes) {
-    if (!this.directedInstance) {
-      this.directedInstance = await this.createInstance();
+      return this.directedInstance.direct(directNodes);
+      // TODO: set related styling here instead of in XibleEditor where it is now
     }
 
-    return this.directedInstance.direct(directNodes);
-    // TODO: set related styling here instead of in XibleEditor where it is now
+    // TODO: simply have XibleEditor set viewState to loadedFlow directly?
+    toJson(...args) {
+      this.removeGlobalConnectors();
+
+      // the viewstate
+      this.setViewState({
+        left: this.editor.left,
+        top: this.editor.top,
+        zoom: this.editor.zoom,
+        backgroundLeft: this.editor.backgroundLeft,
+        backgroundTop: this.editor.backgroundTop
+      });
+
+      return super.toJson(...args);
+    }
   }
 
-  // TODO: simply have XibleEditor set viewState to loadedFlow directly?
-  toJson(...args) {
-    this.removeGlobalConnectors();
-
-    // the viewstate
-    this.setViewState({
-      left: this.editor.left,
-      top: this.editor.top,
-      zoom: this.editor.zoom,
-      backgroundLeft: this.editor.backgroundLeft,
-      backgroundTop: this.editor.backgroundTop
-    });
-
-    return super.toJson(...args);
-  }
-}
+  return XibleEditorFlow;
+})();
