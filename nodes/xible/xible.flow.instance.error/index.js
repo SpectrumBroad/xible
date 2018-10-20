22f12a81eec943cdc490e72e3eebfe3d7bf66d0a
Jong <Stefan.de.Jong.wia@ing.com>
2018-10-20 17:54:33 +0200

    providing state to NODE.error()

--- nodes/xible/xible.flow.instance.error/index.js
+++ nodes/xible/xible.flow.instance.error/index.js
@@ -3,6 +3,6 @@
 module.exports = (NODE) => {
   const triggerIn = NODE.getInputByName('trigger');
   triggerIn.on('trigger', (conn, state) => {
-    NODE.error(new Error(NODE.data.errorMessage || 'unspecified'));
+    NODE.error(new Error(NODE.data.errorMessage || 'unspecified'), state);
   });
 };
